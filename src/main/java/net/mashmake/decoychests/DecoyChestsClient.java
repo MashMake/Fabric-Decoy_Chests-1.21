51a7e1727709533969984e96189e529b8ecd8518
MashMake <33731237+MashMake@users.noreply.github.com>
2024-10-02 14:17:55 +0300

    Initial commit

---
+++ src/main/java/net/mashmake/decoychests/DecoyChestsClient.java
@@ -0,0 +1,10 @@
+package net.mashmake.decoychests;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DecoyChestsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
