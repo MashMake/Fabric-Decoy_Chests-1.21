e3e9052c724bf62c74fd8ecd87c957295b7345f5
KovalenkoIlia <kovalenko.ilia@Inbox.ru>
2024-10-05 16:49:45 +0300

    <3

--- src/main/java/net/mashmake/decoychests/item/ModItems.java
+++ src/main/java/net/mashmake/decoychests/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.mashmake.decoychests.item;
 
+import net.mashmake.decoychests.DecoyChests;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+
+
+
+    private static Item RegisterItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(DecoyChests.MOD_ID, name), item);
+    }
+
+    public static void RegisterModItems() {
+        DecoyChests.LOGGER.info("Registering items for " + DecoyChests.MOD_ID);
+    }
 }
